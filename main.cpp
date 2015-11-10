4fc7487233a8a6e5891674d1d1410c05b7a4a74a
Myzo <myzo@users.noreply.github.com>
2015-11-10 15:49:07 +1100

    adding numbers demo

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  
+  int x, y, answer;
+  
+  cin >> x >> y;
+  answer = x + y;
+  cout << answer;
+  
+  return 0;
+}

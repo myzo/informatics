c8567625278b3827ee59dc229f9922b757f61585
Myzo <myzo@users.noreply.github.com>
2015-11-24 14:30:08 +1100

    Finished game1

--- game1.cpp
+++ game1.cpp
@@ -11,13 +11,15 @@ int N, b[111], c[111][111], s;
 
 int z(int x, int y) {
     if (c[x][y]) return c[x][y];
-    if (x == y) c[x][y] = 0;
+    if (x > y) c[x][y] = 0;
     else if (y - x == 1) c[x][y] = max(b[x], b[y]);
     else c[x][y] = max(b[x] + min(z(x + 1, y - 1), z(x + 2, y)), b[y] + min(z(x + 1, y - 1), z(x, y - 2)));
     return c[x][y];
 }
 
 int main() {
+	freopen("game1.in", "r", stdin);
+	freopen("game1.out", "w", stdout);
     scanf("%d", &N);
     for (int i = 0; i < N; i++) scanf("%d", &b[i]), s += b[i];
     z(0, N - 1);

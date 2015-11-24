6d1dd62b4e6284a76569b6400424ecd82eb1dd08
Myzo <myzo@users.noreply.github.com>
2015-11-24 13:55:35 +1100

    Tested game1

--- game1.cpp
+++ game1.cpp
@@ -1,3 +1,9 @@
+/*
+ID:
+PROG: game1
+LANG: C++
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -6,7 +12,7 @@ int N, b[111], c[111][111], s;
 int z(int x, int y) {
     if (c[x][y]) return c[x][y];
     if (x == y) c[x][y] = 0;
-    else if (abs(x - y) == 1) c[x][y] = max(b[x], b[y]);
+    else if (y - x == 1) c[x][y] = max(b[x], b[y]);
     else c[x][y] = max(b[x] + min(z(x + 1, y - 1), z(x + 2, y)), b[y] + min(z(x + 1, y - 1), z(x, y - 2)));
     return c[x][y];
 }
@@ -15,5 +21,5 @@ int main() {
     scanf("%d", &N);
     for (int i = 0; i < N; i++) scanf("%d", &b[i]), s += b[i];
     z(0, N - 1);
-    printf("%d %d\n", c[0][N], s - c[0][N]);
+    printf("%d %d\n", c[0][N - 1], s - c[0][N - 1]);
 }
